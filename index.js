549558ed15b4f68af7560ed7d714a60ad3d1d27e
Radomir Radionov <66359081+radomir-radionov@users.noreply.github.com>
2021-02-21 15:27:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ let name = form.querySelector(".name");
 let age = form.querySelector(".age");
 let dateOfBirth = form.querySelector(".dateOfBirth");
 let education = form.querySelector(".education");
+let gender = form.querySelector(".gender");
 let fields = form.querySelectorAll(".field");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  const nameValue = document.querySelector(".nameValue");
+  nameValue.innerHTML = name.value;
+  const ageValue = document.querySelector(".ageValue");
+  ageValue.innerHTML = age.value;
+  const dateOfBirthValue = document.querySelector(".dateOfBirthValue");
+  dateOfBirthValue.innerHTML = dateOfBirth.value;
+  const educationValue = document.querySelector(".educationValue");
+  educationValue.innerHTML = education.value;
+  const genderValue = document.querySelector('input[name="rate"]:checked')
+    .value;
+  genderValue.innerHTML = gender.value;
+
+  // document.querySelector('input[name="gender"]:checked').value;
+
   let errors = form.querySelectorAll(".error");
   for (let i = 0; i < errors.length; i++) {
     errors[i].remove();
@@ -16,7 +31,6 @@ form.addEventListener("submit", function (event) {
 
   for (let i = 0; i < fields.length; i++) {
     if (!fields[i].value) {
-      console.log("This field is required", fields[i]);
       let error = document.createElement("div");
       error.className = "error";
       error.style.color = "gray";
@@ -25,4 +39,30 @@ form.addEventListener("submit", function (event) {
       fields[i].after(error);
     }
   }
+  console.log(fields.length);
 });
+
+// -----MODAL-----
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  if (fields[0].value) {
+    modal.style.display = "block";
+  }
+};
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
